bc500da39726f087726bf15288e86e21e2937525
Njinda Salome <salomenjinda0@gmail.com>
2025-09-28 23:40:33 +0100

    added more pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
-import React from 'react';  
+import React from 'react';
 
- import RegistrationForm from './Pages/Register/Register'
+import RegistrationForm from './Pages/Register/Register'
 import LoginForm from './Pages/Login/Login'
-import {  Routes, Route,Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from './Pages/Home/Home';
-import  ContactForm from "./Pages/Contact-Us/ContactForm"
-import AboutUs from "./Pages/About-Us/AboutUs"
+import ContactForm from "./Pages/Contact-Us/ContactForm";
+import AboutUs from "./Pages/About-Us/AboutUs";
+import Community from './Pages/Community/community';
+import Partners from './Pages/Partners/partners';
+import Explore from './Pages/Explore/Explore';
 
 function App() {
 
@@ -13,24 +16,27 @@ function App() {
   return (
     <>
       <div>
-    
-    <Routes>
-      {/* redirect root to /register */}
-       <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} /> 
-        <Route path="/register" element={<RegistrationForm />} />
-        <Route path="/login" element={<LoginForm />} />
-         <Route path="/contact-form" element={<ContactForm />} />
-          <Route path="/about-us" element={<AboutUs/>} />
 
- 
-    </Routes>
+        <Routes>
+          {/* redirect root to /register */}
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/register" element={<RegistrationForm />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/contact-form" element={<ContactForm />} />
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/community" element={<Community />} />
+           <Route path="/partners" element={<Partners />} /> 
+           <Route path="/explore" element={<Explore />} />
+
+
+        </Routes>
 
 
       </div>
-     
-      
-     
+
+
+
     </>
   )
 }
---
+++ frontend/src/Pages/Explore/Explore.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+
+export default function Explore() {
+  return (
+    <div className="min-h-screen flex items-center justify-center px-4">
+      <h2 className="text-2xl sm:text-3xl font-semibold text-center">
+        COMING SOON
+      </h2>
+    </div>
+  );
+}
