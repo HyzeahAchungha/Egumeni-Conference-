1831ff7dc972b394675f76b3f251a5d26d111082
kuku67011 <hyzeala28@gmail.com>
2025-09-29 01:18:34 +0100

    add communities

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,8 +6,8 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Home from './Pages/Home/Home';
 import ContactForm from "./Pages/Contact-Us/ContactForm";
 import AboutUs from "./Pages/About-Us/AboutUs";
-import Community from './Pages/Communities/Communities';
-import Partners from './Pages/Partners/partners';
+import Communities from './Pages/Communities/Communities';
+import Partner from './Pages/Partner/Partner';
 import Explore from './Pages/Explore/Explore';
 
 function App() {
@@ -25,8 +25,8 @@ function App() {
           <Route path="/home" element={<Home />} />
           <Route path="/contact-form" element={<ContactForm />} />
           <Route path="/about-us" element={<AboutUs />} />
-          <Route path="/community" element={<Community />} />
-           <Route path="/partners" element={<Partners />} /> 
+          <Route path="/community" element={<Communities />} />
+           <Route path="/Partner" element={<Partner />} /> 
            <Route path="/explore" element={<Explore />} />
 
 
--- frontend/src/Pages/Partner/Partner.jsx
+++ frontend/src/Pages/Partner/Partner.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 
 
-export default function Partners() {
+export default function Partner() {
   return (
     <div className="min-h-screen flex items-center justify-center px-4">
       <h2 className="text-2xl sm:text-3xl font-semibold text-center">
